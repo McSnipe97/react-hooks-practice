47b05ef1ccb6d132855b079942b41d89286b0dd5
Anuj Soni <anujsoni1997@gmail.com>
2020-11-11 13:23:13 +0530

    Initial Commit to add template files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import State from "./State";
+import Effect from "./Effect";
+import Context from "./Context";
+import Ref from "./Ref";
+import Reducer from "./Reducer";
+import Memo from "./Memo";
+import Callback from "./Callback";
+import LayoutEffect from "./LayoutEffect";
+import ImperativeHandle from "./ImperativeHandle";
+
+import "../public/styles.css";
+
+function App() {
+  return <h1>Placeholder</h1>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
